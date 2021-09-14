60d38ceace750a8c3fb801b53fe5582a4fb43e5d
yuning <yuning>
2021-09-14 17:51:09 +0800

    再调整代码

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def build_files(data_path, tokenized_data_path, num_pieces, full_tokenizer, min_
     with open(data_path, 'r', encoding='utf8') as f:
         print('reading lines')
         lines = json.load(f)
-        lines = [line.replace('\n', ' [SEP] ') for line in lines]  # 用[SEP]表示换行, 段落之间使用SEP表示段落结束
+        lines = [line.replace('\n', '[SEP]') for line in lines]  # 用[SEP]表示换行, 段落之间使用SEP表示段落结束
     all_len = len(lines)
     if not os.path.exists(tokenized_data_path):
         os.mkdir(tokenized_data_path)
@@ -24,7 +24,6 @@ def build_files(data_path, tokenized_data_path, num_pieces, full_tokenizer, min_
         sublines = lines[all_len // num_pieces * i: all_len // num_pieces * (i + 1)]
         if i == num_pieces - 1:
             sublines.extend(lines[all_len // num_pieces * (i + 1):])  # 把尾部例子添加到最后一个piece
-        print('i==={}, sublines==={}'.format(i, sublines))
         sublines = [full_tokenizer.tokenize(line) for line in sublines if
                     len(line) > min_length]  # 只考虑长度超过min_length的句子
         sublines = [full_tokenizer.convert_tokens_to_ids(line) for line in sublines]
